046e71592807bf3c148027950c4273978de4deb9
Scarlet <66797411+TanZeus@users.noreply.github.com>
2023-05-12 20:20:49 +0530

    Add files via upload

--- api.py
+++ api.py
@@ -1,7 +1,6 @@
-import json
 import random
 import string
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__, subdomain_matching=True)
 
@@ -9,14 +8,21 @@ app = Flask(__name__, subdomain_matching=True)
 # 127.0.0.1 localhost exfusiondom.com api.exfusiondom.com
 
 app.config['SERVER_NAME']= "exfusiondom.com:5000"
-@app.route('/<int:parameter>', subdomain='api' ,methods=['GET'])
+@app.route('/<int:parameter>', subdomain='api', methods=['GET'])
+
 
 def gen_rand_string(parameter):
     if parameter < 0 or parameter > 9:
-        return json.dumps({"success":False, "payload":"Invalid parameter. Parameter must be between 0 and 9."}, indent=4)
+        resp=jsonify([{"success":False, "payload":"Invalid parameter. Parameter must be between 0 and 9."}])
+        resp.headers.add('Access-Control-Allow-Origin', '*')
+        resp.headers.add('Access-Control-Allow-Methods', 'GET')
+        return jsonify(resp)
     
     res=''.join(random.choices(string.ascii_letters + string.digits, k=parameter))
-    return json.dumps({"success":True, "payload":res}, indent=4)
+    resp=jsonify([{"success":True, "payload":res}])
+    resp.headers.add('Access-Control-Allow-Origin', '*')
+    resp.headers.add('Access-Control-Allow-Methods', 'GET')
+    return resp
 
 if __name__ == '__main__':
     app.run()
